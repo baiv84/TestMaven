62a214f3a094f1c7cc2b8751ad39bc5cb27b1b92
baiv84 <65267292+baiv84@users.noreply.github.com>
2025-01-30 17:12:11 +0700

    First commit

---
+++ src/main/java/com/apollo84/TestMaven/MainApp.java
@@ -0,0 +1,11 @@
+package com.apollo84.TestMaven;
+
+import com.apollo84.TestMaven.tools.SomeClass;
+
+public class MainApp {
+    public static void main(String[] args) {
+        SomeClass obj = new SomeClass();
+        obj.echo();
+
+    }
+}
---
+++ src/main/java/com/apollo84/TestMaven/tools/SomeClass.java
@@ -0,0 +1,8 @@
+package com.apollo84.TestMaven.tools;
+
+//Some class
+public class SomeClass {
+    public void echo() {
+        System.out.println(this.getClass().getName());
+    }
+}
