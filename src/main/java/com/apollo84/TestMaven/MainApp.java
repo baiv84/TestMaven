3c834b9c8af5803aee44c3b4b8e86844f54827d6
baiv84 <65267292+baiv84@users.noreply.github.com>
2025-01-30 17:43:34 +0700

    Feature1 - step2

--- src/main/java/com/apollo84/TestMaven/MainApp.java
+++ src/main/java/com/apollo84/TestMaven/MainApp.java
@@ -3,7 +3,7 @@ package com.apollo84.TestMaven;
 import com.apollo84.TestMaven.tools.SomeClass;
 
 
-//Feature 1 Main App version
+//Feature 1 Main App version!
 public class MainApp {
     public static void main(String[] args) {
         SomeClass obj = new SomeClass();
--- src/main/java/com/apollo84/TestMaven/tools/Feature1.java
+++ src/main/java/com/apollo84/TestMaven/tools/Feature1.java
@@ -2,4 +2,7 @@ package com.apollo84.TestMaven.tools;
 
 public class Feature1 {
     private Feature1(){}
+    public static void printInfo(){
+        System.out.println("print info");
+    }
 }
